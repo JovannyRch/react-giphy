1ec3bbdbe4ca4b55b991c75660d1cc006cdc7e81
JovannyRch <43225215+JovannyRch@users.noreply.github.com>
2020-07-04 10:37:33 -0500

    Docs folder added

---
+++ docs/precache-manifest.642e185295831bfc4eaa9041b61f993e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42f05568f841d7b9018e6ec32d83c751",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da9ad99055d066e19a75",
+    "url": "/static/css/main.447e83a4.chunk.css"
+  },
+  {
+    "revision": "71522f13fd6174d0aef1",
+    "url": "/static/js/2.c90e9a8e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c90e9a8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da9ad99055d066e19a75",
+    "url": "/static/js/main.536ed76f.chunk.js"
+  },
+  {
+    "revision": "c8429c3d75b971d53245",
+    "url": "/static/js/runtime-main.7b6f9e01.js"
+  }
+]);
